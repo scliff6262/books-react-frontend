6bf988d99ece0c527fc30d509f25e23e06abfd98
scliff6262 <sclifford6262@gmail.com>
2018-08-03 14:51:32 -0400

    setup Form component with Redux

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 
-class Form extends Component {
-
-
-  render(){
-    return(
-      <form onSubmit={this.props.handleSubmit}>
-        <input type='text' placeholder="Title" onChange={this.props.handleChange} value={this.props.state.searchTerm}/>
-        <input type='submit' value='Search'/>
-      </form>
-    )
-  }
+const Form = (props) => {
+  return(
+    <form onSubmit={props.handleSubmit}>
+      <input type='text' placeholder="Title" onChange={props.handleChange} value={props.state.searchTerm}/>
+      <input type='submit' value='Search'/>
+    </form>
+  )
 }
 
 export default Form
