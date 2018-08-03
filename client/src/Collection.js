e0206f6436e975c55afb67c13b317b04e602c9a9
scliff6262 <sclifford6262@gmail.com>
2018-08-03 14:51:05 -0400

    setup Collection componeent with Redux

--- client/src/Collection.js
+++ client/src/Collection.js
@@ -1,55 +1,40 @@
 import React, { Component } from 'react'
 import MyBook from './MyBook'
+import { fetchMyBooks, deleteFromMyBooks } from './actions/myBookActions'
+import { connect } from 'react-redux'
+
 
 class Collection extends Component {
   constructor(){
     super()
-
     this.state = {
-      searchTerm: "",
-      myBooks: []
+      searchTerm: ""
     }
   }
 
     componentDidMount(){
-      fetch('/api/books')
-      .then( r => r.json() )
-      .catch( e => console.log(e) )
-      .then( json => this.setState({ myBooks: json }) )
+      this.props.fetchMyBooks()
     }
 
     handleChange = (e) => {
-      e.persist()
+      e.preventDefault()
       this.setState({ searchTerm: e.target.value})
     }
 
     handleClick = (e) => {
-      e.persist()
       const bookId = e.target.parentElement.getAttribute("book-id")
-      fetch('/api/books/' + bookId, {
-        method: 'delete',
-        body: {},
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-      }).then( r => r.json() )
-      .then( json => {
-        const books = this.state.myBooks.filter( book => book.id !== parseInt(bookId))
-        this.setState({ myBooks: books })
-        alert(json.title + " has been removed from your collection.")
-      })
+      this.props.deleteFromMyBooks(bookId)
     }
 
   render(){
     const searchTermLowerCase = this.state.searchTerm.toLowerCase()
-    const collectionList = this.state.myBooks.filter(book => {
+    const collectionList = this.props.myBooks.filter(book => {
         return book.title.toLowerCase().includes(searchTermLowerCase) || book.author.toLowerCase().includes(searchTermLowerCase)
       }).map(book => {
         return <MyBook key={book.id} book={book} handleClick={this.handleClick}/>
       })
 
-    if(this.state.myBooks.length > 0){
+    if(this.props.myBooks.length > 0){
       return(
         <div>
           <form>
@@ -70,4 +55,8 @@ class Collection extends Component {
 
 }
 
-export default Collection
+const mapStateToProps = (state) => ({
+  myBooks: state.myBooks.myBooks
+})
+
+export default connect(mapStateToProps, { fetchMyBooks, deleteFromMyBooks })(Collection)
